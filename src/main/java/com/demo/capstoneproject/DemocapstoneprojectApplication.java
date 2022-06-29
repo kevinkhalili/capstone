45222978bd2881315e6f0a53506d3f26a2cab75b
Kevin K <kevinkhalili@hotmail.com>
2022-06-29 14:09:58 -0700

    first commit - first model, Book has created

---
+++ src/main/java/com/demo/capstoneproject/DemocapstoneprojectApplication.java
@@ -0,0 +1,13 @@
+package com.demo.capstoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocapstoneprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocapstoneprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo/capstoneproject/model/Book.java
@@ -0,0 +1,76 @@
+package com.demo.capstoneproject.model;
+
+
+import com.sun.istack.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "book")
+
+public class Book {
+
+    //Creating primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    ////////////////////////WARNING WARNING WARNING//////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
+    //************THE ANNOTATION FOR THESE FIELDS NEED TO BE MODIFIED FOR VALIDATION LATER
+    //////////////////////////////////////////////////////////////////////////////////////
+    //****************SO AS TABLE RELATIONSHIP***************************************
+
+    //Creating other fields of book table such as title, tag, authors, publisher, isbn, status and create_date
+    @NotNull
+    @Column(name = "title")
+    String title;
+
+    @NotNull
+    @Column(name = "tag")
+    String tag;
+
+    @NotNull
+    @Column(name = "authors")
+    String authors;
+
+
+    @Column(name = "publisher")
+    String publisher;
+
+
+    @Column(name = "isbn")
+    String isbn;
+
+
+    @Column(name = "status")
+    String status;
+
+    @Column(name = "create_date")
+    Date createDate;
+
+    //overriding equals method
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && title.equals(book.title) && tag.equals(book.tag) && authors.equals(book.authors) && Objects.equals(publisher, book.publisher) && Objects.equals(isbn, book.isbn) && Objects.equals(status, book.status) && Objects.equals(createDate, book.createDate);
+    }
+
+    //overriding hashCode method
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, tag, authors, publisher, isbn, status, createDate);
+    }
+}
