6ea7ce862d2c163929242f5837ce1cd8eaf0b9df
Kevin K <kevinkhalili@hotmail.com>
2022-06-30 10:32:38 -0700

    second commit - second model, Category has been populated

--- src/main/java/com/demo/capstoneproject/model/Category.java
+++ src/main/java/com/demo/capstoneproject/model/Category.java
@@ -1,4 +1,56 @@
 package com.demo.capstoneproject.model;
 
+
+import com.sun.istack.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter @Setter @ToString @Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "category")
+
 public class Category {
+
+    //Declaring data members with annotations
+
+    //*************Warning**************
+    //*************Warning**************
+    /////////////Annotations need to be modified for validation///////////
+    ///////////////////////////////////////////////////////////////////////
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @NotNull
+    /////////////////////////Ask Jafer///////////
+//  @NotBlank
+    @Column(name = "name")
+    String name;
+
+    @NotNull
+//    @NotBlank
+
+//     @Length(max = 4, message = "*Must not exceed 4 characters.")
+    @Column(name = "short_name")
+    String shortName;
+
+    @Column(name = "notes")
+//    @Length(max = 1000, message = "*Must not exceed 1000 characters.")
+    String notes;
+
+    @Column(name = "create_date")
+    Date createDate;
+
+/////////////////////////Table relationship here////////////////
+
 }
