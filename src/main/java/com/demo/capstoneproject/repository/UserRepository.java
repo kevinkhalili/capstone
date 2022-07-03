5dc83096e73dc02a65d941536681fd8a8377d5f0
Kevin K <kevinkhalili@hotmail.com>
2022-07-03 08:41:39 -0700

    UserRepository created

---
+++ src/main/java/com/demo/capstoneproject/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.demo.capstoneproject.repository;
+
+import com.demo.capstoneproject.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public List<User> findAllByOrderByDisplayNameAsc();
+
+    public List<User> findAllByActiveOrderByDisplayNameAsc(Integer active);
+
+    //////////////////check the problem later///////////////////////
+
+//    public User findByUserName(String username);
+}
